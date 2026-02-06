f96f942f038b4c2ba322835a7cb3dfc9cb2ba4a1
fragadmsc <damasceno.lucas512@gmail.com>
2026-02-06 12:58:10 -0300

    created header files from library

--- lib/protocols/prot.h
+++ lib/protocols/prot.h
@@ -1,1 +1,12 @@
-int initialize_mpu(void);
+#pragma once
+
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+using led_strip = Adafruit_NeoPixel;
+using mpu_sensor = mpu_sensor;
+
+int initialize_mpu(mpu_sensor& mpu);
+int initialize_leds(led_strip& pixels);
+int initialize_serial(HardwareSerial& serial);
