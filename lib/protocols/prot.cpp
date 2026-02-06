4fd2566d1a82f7056fff28974a14c2e32a5ceaca
fragadmsc <damasceno.lucas512@gmail.com>
2026-02-06 14:01:39 -0300

    added coments

--- lib/protocols/prot.cpp
+++ lib/protocols/prot.cpp
@@ -1,6 +1,7 @@
 #include "prot.h"
 
 int position_to_led(float position) {
+    //given the position of the ball, it finds the led correspondent to it
     float ratio = position/BAR_SIZE;
     return floor(ratio*NUM_LEDS);
 }
