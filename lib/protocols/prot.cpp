7d459b2b8f91345850975b3b68a38bdf50574366
fragadmsc <damasceno.lucas512@gmail.com>
2026-02-09 09:04:27 -0300

    adicionado returno dos initializes

--- lib/protocols/prot.cpp
+++ lib/protocols/prot.cpp
@@ -10,13 +10,18 @@ int initialize_mpu(mpu_sensor& mpu){
     mpu.begin();
     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
     mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    return 0;
 }
 
 int initialize_leds(led_strip& pixels){
     pixels.begin();
+    pixels.clear();
+    pixels.show();
+    return 0;
 }
 
 int initialize_serial(HardwareSerial& serial){
     Serial.begin(115200);
+    return 0;
 }
 
